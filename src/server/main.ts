1d82eeef92d1b1584cdcbe0eb9affa3d17e0c501
Juri <juri.strumpflohner@r3-gis.com>
2018-10-10 14:03:11 +0200

    fix: increase bodyParser size

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // bodyParser: false,
+  });
+  app.use(json({ limit: '50mb' }));
   await app.listen(3000);
 }
 bootstrap();
